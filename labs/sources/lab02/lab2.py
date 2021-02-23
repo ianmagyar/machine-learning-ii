b48e959a8cd587fdd0c684808e643957fed1e70e
ianmagyar <jmagyar1993@gmail.com>
2021-02-23 13:53:03 +0100

    updated lab 2, added sample solution

---
+++ labs/sources/lab02/lab2.py
@@ -0,0 +1,18 @@
+class Gridworld:
+    def __init__(self, height, width, goal_position=None, walls=[], traps=[]):
+        pass
+
+    def reset(self):
+        return
+    
+    def calculate_reward(self, new_state):
+        return
+    
+    def is_done(self):
+        return
+    
+    def step(self, action):
+        return
+    
+    def render(self):
+        pass
